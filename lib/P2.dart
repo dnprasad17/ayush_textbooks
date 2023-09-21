3ad61ae208efdb46ee98c61330b68e44fc2c2e50
Dev Narayan Prasad <dnprasad17@gmail.com>
2023-09-22 00:22:39 +0530

    Lets start

---
+++ lib/P2.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class P2 extends StatelessWidget {
+  const P2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Select the Year'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IntrinsicWidth(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Add your onPressed logic here
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue, // Background color
+                    onPrimary: Colors.white, // Text color
+                    elevation: 5, // Elevation
+                    padding: EdgeInsets.all(8), // Padding
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.book), // Book icon
+                      SizedBox(
+                          width: 8), // Add some space between icon and text
+                      Text('Year 1', style: TextStyle(fontSize: 30)), // Text
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              IntrinsicWidth(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Add your onPressed logic here
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue, // Background color
+                    onPrimary: Colors.white, // Text color
+                    elevation: 5, // Elevation
+                    padding: EdgeInsets.all(8), // Padding
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.book), // Book icon
+                      SizedBox(
+                          width: 8), // Add some space between icon and text
+                      Text('Year 2', style: TextStyle(fontSize: 30)), // Text
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              IntrinsicWidth(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Add your onPressed logic here
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue, // Background color
+                    onPrimary: Colors.white, // Text color
+                    elevation: 5, // Elevation
+                    padding: EdgeInsets.all(8), // Padding
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.book), // Book icon
+                      SizedBox(
+                          width: 8), // Add some space between icon and text
+                      Text('Year 3', style: TextStyle(fontSize: 30)), // Text
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              IntrinsicWidth(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Add your onPressed logic here
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue, // Background color
+                    onPrimary: Colors.white, // Text color
+                    elevation: 5, // Elevation
+                    padding: EdgeInsets.all(8), // Padding
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.book), // Book icon
+                      SizedBox(
+                          width: 8), // Add some space between icon and text
+                      Text('Year 4', style: TextStyle(fontSize: 30)), // Text
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:ayush_textbook/P1.dart';
+import 'package:ayush_textbook/P2.dart';
+import 'package:ayush_textbook/P3.dart';
+import 'package:ayush_textbook/P4.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     initialRoute: 'P1',
     debugShowCheckedModeBanner: false,
-    routes: {'P1': (context) => P1() },
+    routes: {'P1': (context) => P1(), 'P2': (context) => P2(), 'P3' :(context) => P3(),'P4': (context) => P4() },
   ));
 }
 
