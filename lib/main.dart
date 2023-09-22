bc53c98c048395a348863c3ca7e4cc55d0965533
Dev Narayan Prasad <dnprasad17@gmail.com>
2023-09-22 23:20:40 +0530

    yo2

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,14 @@ import 'package:ayush_textbook/P2.dart';
 import 'package:ayush_textbook/P3.dart';
 import 'package:ayush_textbook/P4.dart';
 import 'package:ayush_textbook/P5.dart';
+import 'package:ayush_textbook/phone.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: 'P1',
     debugShowCheckedModeBanner: false,
-    routes: {'P1': (context) => P1(), 'P2': (context) => P2(), 'P3' :(context) => P3(),'P4': (context) => P4(), 'P5': (context) => P5(),},
+    initialRoute: 'phone',
+    routes: {'phone': (context) => MyPhone() , 'P1': (context) => P1(), 'P2': (context) => P2(), 'P3' :(context) => P3(),'P4': (context) => P4(), 'P5': (context) => P5(),},
   ));
 }
 
